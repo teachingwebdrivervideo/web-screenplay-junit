d5bcba4008b1b271f5455073db7550e3a0e3da60
Jacob Vu <vuthelinh@gmail.com>
2019-08-24 15:36:10 +0700

    Init web test project using screenplay pattern

---
+++ README.md
@@ -0,0 +1,1 @@
+"# web-screenplay-junit" 
---
+++ src/test/java/vn/selenium/teaching/features/adding/AddingNewSomethingStory.java
@@ -0,0 +1,50 @@
+package vn.selenium.teaching.features.adding;
+
+import static net.serenitybdd.screenplay.EventualConsequence.eventually;
+import static net.serenitybdd.screenplay.GivenWhenThen.givenThat;
+import static net.serenitybdd.screenplay.GivenWhenThen.seeThat;
+import static net.serenitybdd.screenplay.GivenWhenThen.then;
+import static net.serenitybdd.screenplay.GivenWhenThen.when;
+import static org.hamcrest.Matchers.containsString;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+
+import net.serenitybdd.junit.runners.SerenityRunner;
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.abilities.BrowseTheWeb;
+import net.serenitybdd.screenplay.questions.page.TheWebPage;
+import net.thucydides.core.annotations.Managed;
+import net.thucydides.core.annotations.Steps;
+import vn.selenium.teaching.tasks.OpenTheApplication;
+import vn.selenium.teaching.tasks.Search;
+
+@RunWith(SerenityRunner.class)
+public class AddingNewSomethingStory {
+
+    Actor anna = Actor.named("Anna");
+
+    @Managed(uniqueSession = true)
+    public WebDriver herBrowser;
+
+    @Steps
+    OpenTheApplication openTheApplication;
+
+    @Before
+    public void annaCanBrowseTheWeb() {
+        anna.can(BrowseTheWeb.with(herBrowser));
+    }
+
+    @Test
+    public void search_results_should_show_the_search_term_in_the_title() {
+
+        givenThat(anna).wasAbleTo(openTheApplication);
+
+        when(anna).attemptsTo(Search.forTheTerm("BDD In Action"));
+
+        then(anna).should(eventually(seeThat(TheWebPage.title(), containsString("BDD In Action"))));
+
+    }
+}
